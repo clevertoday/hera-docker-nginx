8463edafa6b3da7ab773a3018cf1ddcbee8d2873
Brice Argenson <bargenson@gmail.com>
2015-11-25 12:23:35 -0500

    Add environment variables for services hosts and ports

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,53 @@
 #!/bin/bash
+
 set -e
+
+
+function setConfiguration() {
+  KEY=$1
+  VALUE=$2
+  sed -i "s/{{$KEY}}/$VALUE/g" /etc/nginx/nginx.conf
+}
+
+
+if [ -n "${GRAFANA_HOST+1}" ]; then
+  setConfiguration "GRAFANA_HOST" "$GRAFANA_HOST"
+fi
+
+if [ -n "${GRAFANA_PORT+1}" ]; then
+  setConfiguration "GRAFANA_PORT" "$GRAFANA_PORT"
+else
+  setConfiguration "GRAFANA_PORT" "80"
+fi
+
+
+if [ -n "${GRAPHITE_HOST+1}" ]; then
+  setConfiguration "GRAPHITE_HOST" "$GRAPHITE_HOST"
+fi
+
+if [ -n "${GRAPHITE_PORT+1}" ]; then
+  setConfiguration "GRAPHITE_PORT" "$GRAPHITE_PORT"
+else
+  setConfiguration "GRAPHITE_PORT" "80"
+fi
+
+
+if [ -n "${ES_HOST+1}" ]; then
+  setConfiguration "ES_HOST" "$ES_HOST"
+fi
+
+if [ -n "${ES_PORT+1}" ]; then
+  setConfiguration "ES_PORT" "$ES_PORT"
+else
+  setConfiguration "ES_PORT" "9200"
+fi
+
 
 if [[ "$1" == -* ]]; then
     set -- /usr/sbin/nginx "$@"
 elif [ -z "$1" ]; then
     exec /usr/sbin/nginx
 fi
+
 
 exec "$@"
